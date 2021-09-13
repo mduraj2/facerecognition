c7740620b9c425a4fb2121dc4ff26358eb382a45
mirek <m.duraj2@wp.pl>
2021-09-13 21:23:07 +0200

    uploading files

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input })
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://secret-inlet-36668.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://secret-inlet-36668.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
