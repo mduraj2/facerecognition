f96c3df9d04163e6a8a477741b29b10df5523f1b
mirek <m.duraj2@wp.pl>
2021-09-13 21:22:24 +0200

    updating path

--- src/components/SingIn/SingIn.js
+++ src/components/SingIn/SingIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin', {
+        fetch('https://secret-inlet-36668.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
